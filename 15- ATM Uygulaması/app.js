7d5500a964261a29b46fa060b49edf51588468ab
busraygul <aygul.busra58@gmail.com>
2025-06-07 23:37:29 +0300

    switch-case bitiş

--- 15- ATM Uygulaması/app.js
+++ 15- ATM Uygulaması/app.js
@@ -1,25 +1,35 @@
 let bakiye = 1000;
 let metin = prompt("1-Bakiye Görüntüleme \r\n2-Para Çekme \r\n3-Para Yatırma\r\n4-Çıkış\r\nLütfen bir değer seçiniz!");
 
-let deger = prompt(metin);
+let deger = metin;
 
 switch(deger){
     case "1": 
         alert("Bakiyeniz: "+bakiye);
-        alert(metin);
-    break;
+        break;
 
     case "2":
         let cekilecekTutar = Number(prompt("Çekilecek  tutarı giriniz."));
-        bakiye = bakiye - cekilecekTutar;
-
-    break;
+        if(cekilecekTutar<bakiye){
+            //başarılı
+            bakiye = bakiye - cekilecekTutar;
+            alert("Kalan Bakiye: "+bakiye);
+        }else{
+            alert("Bakiyenizden fazla para çekemezsiniz!\r\nBakiyeniz: "+bakiye + " "+ "Çekilecek Tutar: "+cekilecekTutar);
+        }
+        break;
 
     case "3":
         let yatirilacakTutar = Number(prompt("Yatırılacak tutarı giriniz:"));
         bakiye = bakiye + yatirilacakTutar;
-    break;
+        alert("Güncel Bakiye: "+bakiye);
+        break;
 
     case "4":
         alert("Çıkış yapıldı!");
+        break;
+    
+    default:
+        alert("Lütfen 1 ile 4 arasında bir değer giriniz!");
+        break;
 }
