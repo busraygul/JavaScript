2a5e5ea361a951c4173366b57b16111782632b8b
busraygul <aygul.busra58@gmail.com>
2025-06-08 18:52:56 +0300

    debugger

---
+++ 18-Breakpoint ve Tooltip/app.js
@@ -0,0 +1,16 @@
+// function ortalamaBul(vize1,vize2,vize3){
+
+//     return 75;
+// }
+// ortalamaBul();
+
+debugger; //satırları tek tek incelemek istediğimizde kulllanırız. Nereye koyarsak o satırdan başlar.
+let a = 10;
+let b = 20;
+let c = a+b;
+let d = a+b+c;
+let e = a+c+d;
+
+let sonuc = a+b+c+d+e;
+
+console.log("Sonuç: " +sonuc);
