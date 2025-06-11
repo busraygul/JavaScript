a343983b4ab1c3f0aab50ee2e4f48848e6d10806
busraygul <aygul.busra58@gmail.com>
2025-06-11 17:20:59 +0300

    return

---
+++ 29-Geriye Değer Döndüren Metot Tanımlama/app.js
@@ -0,0 +1,30 @@
+// function cube(sayi){
+//     let sonuc = sayi*sayi*sayi;
+//     console.log(sonuc);
+// }
+// cube(3);
+//Sonuc a buradan erişemem çünkü fonksiyonun içinde kaldı.
+
+function cube(sayi){
+    let sonuc = sayi*sayi*sayi;
+    return sonuc; //returndan sonraki koda erişilemez.
+}
+let donenDeger = cube(2);//return ı yakaladık.
+
+
+function kareAl(sayi){
+    let sonuc = sayi*sayi;
+    console.log(sonuc);
+}
+
+
+console.log(donenDeger);
+
+kareAl(donenDeger);
+//cube de donenDegeri kareAlda çağırdık.
+
+function yazdir(){
+    console.log("Büşra");
+    // void : geriye değer döndürmeyen metotlarda kullanılır. return yok.
+}
+yazdir();
