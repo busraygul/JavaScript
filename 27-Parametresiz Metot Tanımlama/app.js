3f781d6c9fde0314224af022c4663c578c9966c6
busraygul <aygul.busra58@gmail.com>
2025-06-11 16:51:43 +0300

    parametresiz metot

---
+++ 27-Parametresiz Metot Tanımlama/app.js
@@ -0,0 +1,20 @@
+//Parametresiz ve geriye değer döndürmeyen metot tanımlamak
+
+/*
+function methodIsmi(){
+kodlar...
+}
+*/
+
+function yazdir(){
+    console.log("Büşra");
+}
+yazdir();
+yazdir();
+
+
+function topla(){
+    console.log(5+7);
+
+}
+topla();
